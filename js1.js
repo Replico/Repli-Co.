ba445279bc6bcdc339242617f46a83d8657c26f1
Replico <96168920+Replico@users.noreply.github.com>
2021-12-20 07:35:42 -0800

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,16 @@
+function firstButton() {
+    document.getElementById("demo").innerHTML = "Heyo!";
+}
+
+function voteCount() {
+    let x, y , z;
+    
+}
+
+function yesVote() {
+  alert("You voted yes!")
+  }
+
+function noVote() {
+  alert("You voted no!")
+  }
